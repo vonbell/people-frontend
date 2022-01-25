cac4d6e1ad6b951b6ede93d489910f1d3ec2181b
Gr8ness <pmleechapman@gmail.com>
2022-01-25 15:02:39 -0500

    updated header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
-function Header(props){
-    return <h1>Header</h1>;
-  } 
-  
-  export default Header;
+import { Link } from "react-router-dom";
+
+function Header(props) {
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <div>People App</div>
+      </Link>
+    </nav>
+  );
+}
+
+export default Header;
