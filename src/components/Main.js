a36259f173c87f495a3b979acc6cd6a22b436b48
Gr8ness <pmleechapman@gmail.com>
2022-01-26 15:16:06 -0500

    updated index and show, fully functioning front end

--- src/components/Main.js
+++ src/components/Main.js
@@ -31,16 +31,44 @@ function Main(props) {
 
     };
 
+    const updatePeople = async (person, id) => {
+        // make put request to create people
+        await fetch(URL + id, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "Application/json",
+          },
+          body: JSON.stringify(person),
+        });
+        // update list of people
+        getPeople();
+      }
+    
+      const deletePeople = async id => {
+        // make delete request to create people
+        await fetch(URL + id, {
+          method: "DELETE",
+        })
+        // update list of people
+        getPeople();
+      }
+    
+
+    useEffect(() => getPeople(), []);
+
     return (
         <main>
             <Switch>
                 <Route exact path="/">
-                    <Index />
+                    <Index people={people} createPeople={createPeople}/>
                 </Route>
                 <Route
                     path="/people/:id"
                     render={(rp) => (
                         <Show
+                        people = {people}
+                        updatePeople = {updatePeople}
+                        deletePeople = {deletePeople}
                             {...rp}
                         />
                     )}
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,62 @@
-function Show(props){
-    return <h1>Show</h1>;
-  } 
-  
-  export default Show;
+import { useState } from "react";
+function Show(props) {
+  const id = props.match.params.id;
+  const people = props.people;
+  const person = people.find(p => p._id === id);
+
+  const [ editForm, setEditForm ] = useState(person);
+
+  // handleChange function for form
+  const handleChange = event => {
+    setEditForm({ ...editForm, [event.target.name]: event.target.value });
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // you will have an undefined parameter if you do not pass person._id
+    props.updatePeople(editForm, person._id);
+    props.history.push("/");
+  }
+
+  const removePerson = () => {
+    props.deletePeople(person._id);
+    props.history.push("/");
+  }
+
+  return (
+    <div className="person">
+      <h1>{person.name}</h1>
+      <h2>{person.title}</h2>
+      <img src={person.image} alt={person.name} />
+      <button id="delete" onClick={removePerson}>
+        DELETE
+      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={editForm.name}
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.image}
+          name="image"
+          placeholder="image URL"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.title}
+          name="title"
+          placeholder="title"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Update Person" />
+      </form>
+    </div>
+  )
+}
+
+export default Show;
